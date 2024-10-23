c571352dae545d65416a2e1414138b3e232f7267
Choi SeungMyeong <45002556+710csm@users.noreply.github.com>
2024-10-23 14:26:28 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.swift
+++ 프로그래머스/2/42586. 기능개발/기능개발.swift
@@ -6,12 +6,15 @@ func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
     var distribute: Int = 0
     
     for index in 0..<progresses.count {
+        // 100 - progress - speed * 작업일 -> 작업일 = (100 - progress) / speed
+        // 올림을 해주기 위해 ceil을 사용하여 각 작업당 걸리는 시간을 계산한다
+        // ex) [95, 90, 99, 99, 80, 99] -> [5, 10, 1, 1, 20, 1]
         let remain = ceil((100.0 - Double(progresses[index])) / Double(speeds[index]))
         workDay.append(Int(remain))
     }
     
+    // 걸리는 작업일을 비교하여 현재 제일 오래걸리는 작업일보다 작으면 배포 작업의 개수를 +1 해준다
     var maxDay = workDay.first!
-    
     for day in workDay {
         if day > maxDay {
             if distribute > 0 {
