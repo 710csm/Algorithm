2e48bc92900314cdd07bc3c452cf6e81bb701a55
Choi SeungMyeong <45002556+710csm@users.noreply.github.com>
2025-01-07 08:58:00 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 29.64 ms, Memory: 19.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func solution(_ s: String) -> [Int] {
+    var task: Int = 0
+    var totalZeroCount: Int = 0
+    var s = s
+    
+    while true {
+        totalZeroCount += s.filter({ $0 == "0"}).count
+        task += 1
+        
+        let length = s.filter({ $0 == "1"}).count
+        s = String(String(length, radix: 2))
+        
+        if s == "1" {
+            break
+        }
+    }
+    
+    return [task, totalZeroCount]
+}
