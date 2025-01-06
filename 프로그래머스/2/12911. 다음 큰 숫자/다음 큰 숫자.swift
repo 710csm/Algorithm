91ed41a4be81a61d083b23b607cb4fca1eda3557
Choi SeungMyeong <45002556+710csm@users.noreply.github.com>
2025-01-06 09:32:38 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.swift
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.swift
@@ -1,15 +1,11 @@
 import Foundation
 
 func solution(_ n: Int) -> Int {
-    let countOfOne = String(n, radix: 2).filter({ $0 == "1"}).count
-    
     var answer: Int = 0
     var currentNumber = n + 1
     
     while true {
-        let biggerNumber = String(currentNumber, radix: 2).filter({ $0 == "1"}).count
-        
-        if biggerNumber == countOfOne {
+        if n.nonzeroBitCount == currentNumber.nonzeroBitCount {
             answer = currentNumber
             break
         }
