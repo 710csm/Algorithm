cac661f9acf2276bba086428802842cd8aa190f1
Choi SeungMyeong <45002556+710csm@users.noreply.github.com>
2025-01-08 08:57:23 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.19 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func solution(_ s:String) -> String {
+    var result: String = ""
+    
+    let split = s.components(separatedBy: " ")
+    for (index, word) in split.enumerated() {
+        var newWord = word.lowercased()
+        if let first = newWord.first {
+            if first.isLetter {
+                let capitalizedFirstCharacter = String(first).uppercased()
+                newWord.removeFirst()
+                newWord = capitalizedFirstCharacter + newWord
+            }
+        }
+        result += index == 0 ? "\(newWord)" : " \(newWord)"
+    }
+    
+    return result
+}
