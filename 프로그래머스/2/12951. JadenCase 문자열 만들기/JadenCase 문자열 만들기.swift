eb27abbb518a0eca719f8be30109f70f49dcfa94
Choi SeungMyeong <45002556+710csm@users.noreply.github.com>
2025-01-08 08:59:09 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.22 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.swift
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.swift
@@ -6,12 +6,10 @@ func solution(_ s:String) -> String {
     let split = s.components(separatedBy: " ")
     for (index, word) in split.enumerated() {
         var newWord = word.lowercased()
-        if let first = newWord.first {
-            if first.isLetter {
-                let capitalizedFirstCharacter = String(first).uppercased()
-                newWord.removeFirst()
-                newWord = capitalizedFirstCharacter + newWord
-            }
+        if let first = newWord.first, first.isLetter {
+            let capitalizedFirstCharacter = String(first).uppercased()
+            newWord.removeFirst()
+            newWord = capitalizedFirstCharacter + newWord
         }
         result += index == 0 ? "\(newWord)" : " \(newWord)"
     }
