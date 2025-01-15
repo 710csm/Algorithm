2535a71a3ef99bf19061dbe5588814dcf1c7beb5
Choi SeungMyeong <45002556+710csm@users.noreply.github.com>
2025-01-15 09:34:54 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.swift
+++ 0071-simplify-path/0071-simplify-path.swift
@@ -1,46 +1,18 @@
 class Solution {
     func simplifyPath(_ path: String) -> String {
-        var result: String = ""
         var stack: [String] = []
         var pathArr = path.split(separator: "/")
         
-        if pathArr.count == 0 { return "/" }
-        
-        for (index, path) in pathArr.enumerated() {
-            if stack.isEmpty {
-                stack.append("/")
-            }
-            
+        for path in pathArr {
             if path == ".." {
                 if stack.count > 1 {
                     stack.removeLast()
                 }
-                
-                while stack.last != "/" && !stack.isEmpty {
-                    stack.removeLast()
-                }
-            } else if path == "." && stack.last == "/" {
-                continue
-            } else {
+            } else if path != "." {
                 stack.append(String(path))
             }
-            
-            if !stack.isEmpty && stack.last != "/" {
-                stack.append("/")
-            }
-
         }
         
-        if stack.last == "/" && stack.count != 1 {
-            stack.removeLast()
-        }
-        
-        if stack.isEmpty { stack.append("/") }
-        
-        for path in stack {
-            result += path
-        }
-        
-        return result
+        return "/" + stack.joined(separator: "/")
     }
 }
