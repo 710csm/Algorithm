391ec0cbfe2fc971460d29357b8aefd61918922f
Choi SeungMyeong <45002556+710csm@users.noreply.github.com>
2025-02-04 09:26:37 +0900

    [level 3] Title: 단어 변환, Time: 1.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.swift
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.swift
@@ -1,49 +1,45 @@
 import Foundation
 
-class Word {
-    var word: String
-    var count: Int
-
-    init(_ word: String, _ count: Int) {
-        self.word = word
-        self.count = count
-    }
-}
-
-func solution(_ begin: String, _ target: String, _ words: [String]) -> Int {
+func solution(_ begin:String, _ target:String, _ words:[String]) -> Int {
+    let words = [begin] + words
+    let targetIndex = words.firstIndex(of: target)
+    let n = words.count
+    var graph = [[Int]](repeating: [Int](repeating: 0, count: n), count: n)
+    
     if !words.contains(target) {
         return 0
     }
     
-    var result: Int = 0
-    var visited: [Bool] = [Bool](repeating: false, count: words.count)
-    var queue: [Word] = []
-    queue.append(Word(begin, 0))
-    
-    while !queue.isEmpty {
-        let currrent = queue.removeFirst()
-        
-        if currrent.word == target {
-            result = currrent.count
-            break
-        }
-        
-        for i in 0..<words.count {
-            var count: Int = 0
-            let word = words[i]
-            
-            for (index, c) in currrent.word.enumerated() {
-                if c != word[word.index(word.startIndex, offsetBy: index)] {
-                    count+=1
-                }
-            }
-            
-            if count == 1 && !visited[i] {
-                visited[i] = true
-                queue.append(Word(word, currrent.count + 1))
+    for i in 1..<n {
+        for j in i+1...n {
+            if getDistance(words[i-1], words[j-1]) == 1 {
+                graph[i][j] = 1
+                graph[j][i] = 1
             }
         }
     }
     
-    return result
+    func dijkstra(_ start: Int, _ to: Int) -> Int {
+        let INF = Int.max
+        var distance = [Int](repeating: INF, count: n+1)
+        var queue = [start]
+        distance[start] = 0
+        
+        while !queue.isEmpty {
+            let first = queue.removeFirst()
+            for i in 1...n where graph[first][i] != 0 {
+                let cost = distance[first] + 1
+                if distance[i] > cost {
+                    distance[i] = cost
+                    queue.append(i)
+                }
+            }
+        }
+        return distance[to]
+    }
+    return dijkstra(1, targetIndex! + 1)
+}
+
+func getDistance(_ current: String, _ next: String) -> Int {
+    return zip(Array(current), Array(next)).filter {$0 != $1}.count
 }
