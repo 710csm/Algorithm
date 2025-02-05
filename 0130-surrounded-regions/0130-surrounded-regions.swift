28781868b8f3b81edfa98594f6d3476ac3a3b87d
Choi SeungMyeong <45002556+710csm@users.noreply.github.com>
2025-02-05 09:46:17 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.swift
+++ 0130-surrounded-regions/0130-surrounded-regions.swift
@@ -12,7 +12,6 @@ class Solution {
     func solve(_ board: inout [[Character]]) {
         let m = board.count
         let n = board[0].count
-        let directions: [(Int, Int)] = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         var visited: Set<Point> = []
         
         for i in 0..<m {
@@ -20,40 +19,54 @@ class Solution {
                 let currentPoint = Point(x: i, y: j)
                 
                 if board[i][j] == "O" && !visited.contains(currentPoint) {
-                    var isSurrrounded: Bool = true
                     var queue: [Point] = [currentPoint]
-                    var removeQueue: [Point] = [currentPoint]
+                    var surroundedQueue: [Point] = [currentPoint]
                     visited.insert(currentPoint)
                     
-                    while !queue.isEmpty {
-                        let point = queue.removeFirst()
-                        
-                        for direction in directions {
-                            let nx = point.x + direction.0
-                            let ny = point.y + direction.1
-                            
-                            if nx >= 0, nx < m, ny >= 0, ny < n {
-                                let nextPoint = Point(x: nx, y: ny)
-                                if board[nx][ny] == "O" && !visited.contains(nextPoint) {
-                                    queue.append(nextPoint)
-                                    removeQueue.append(nextPoint)
-                                    visited.insert(nextPoint)
-                                }
-                            } else {
-                                isSurrrounded = false
-                                removeQueue.removeAll()
-                            }
-                        }
-                    }
-                    
-                    if isSurrrounded {
-                        while !removeQueue.isEmpty {
-                            let currentPoint = removeQueue.removeLast()
-                            board[currentPoint.x][currentPoint.y] = "X"
-                        }
+                    if findSurroundedRegion(&board, &visited, &queue, &surroundedQueue, m, n) {
+                        removeSurroundedRegion(&board, surroundedQueue)
                     }
                 }
             }
+        }
+    }
+
+    func findSurroundedRegion(
+        _ board: inout [[Character]], _ visited: inout Set<Point>,
+        _ queue: inout [Point], _ surroundedQueue: inout [Point],
+        _ m: Int, _ n: Int
+    ) -> Bool {
+        let directions: [(Int, Int)] = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        
+        while !queue.isEmpty {
+            let point = queue.removeFirst()
+            
+            for direction in directions {
+                let nx = point.x + direction.0
+                let ny = point.y + direction.1
+                
+                if nx >= 0, nx < m, ny >= 0, ny < n {
+                    let nextPoint = Point(x: nx, y: ny)
+                    if board[nx][ny] == "O" && !visited.contains(nextPoint) {
+                        queue.append(nextPoint)
+                        surroundedQueue.append(nextPoint)
+                        visited.insert(nextPoint)
+                    }
+                } else {
+                    surroundedQueue.removeAll()
+                    return false
+                }
+            }
+        }
+        
+        return true
+    }
+
+    func removeSurroundedRegion(_ board: inout [[Character]], _ surroundedQueue: [Point]) {
+        var queue = surroundedQueue
+        while !queue.isEmpty {
+            let currentPoint = queue.removeLast()
+            board[currentPoint.x][currentPoint.y] = "X"
         }
     }
 }
