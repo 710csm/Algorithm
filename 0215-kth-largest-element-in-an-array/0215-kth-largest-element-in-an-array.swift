6b76a0d262effe75f5b6cf041566f010dd452f8f
Choi SeungMyeong <45002556+710csm@users.noreply.github.com>
2025-03-31 15:32:39 +0900

    Time: 2309 ms (10.17%), Space: 20.8 MB (74.92%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.swift
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.swift
@@ -1,6 +1,26 @@
 class Solution {
     func findKthLargest(_ nums: [Int], _ k: Int) -> Int {
-        let sortedNums = nums.sorted(by: >)
-        return sortedNums[k-1]
+        var nums = nums
+        quickSort(&nums, 0, nums.count-1)
+        return nums[nums.count-k]
+    }
+
+    func quickSort(_ nums: inout [Int], _ start: Int, _ end: Int) {
+        if start >= end {
+            return
+        }
+
+        var j = start-1
+        for i in start...end {
+            if nums[i] < nums[end] || i == end {
+                j+=1
+                let temp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = temp
+            }
+        }
+        
+        quickSort(&nums, start, j-1)
+        quickSort(&nums, j+1, end)
     }
 }
