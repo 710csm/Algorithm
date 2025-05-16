81976874733092966f70c736efb120792d60198d
Choi SeungMyeong <45002556+710csm@users.noreply.github.com>
2025-05-16 14:34:48 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
@@ -0,0 +1,14 @@
+var dp: [Int] = Array(repeating: 0, count: 20)
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in 4..<11 {
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+}
+
+let t = Int(readLine()!)!
+for _ in 0..<t {
+    let n = Int(readLine()!)!
+    print(dp[n])
+}
